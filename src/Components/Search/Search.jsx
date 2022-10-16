1c896819e9c00ef58d07e8507c2839749cd23238
Angeles Gallardo <angelesgallardo.mlg@gmail.com>
2022-10-16 05:40:23 -0300

    corregido bug de search con paginado

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,34 +1,7 @@
 //---------------IMPORTS---------------//
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { hotelByName } from '../../redux/action/action';
-//import "./Styles.css";
-
-//--------------FUNCTION-----------------//
-/* export default function Search() {
-  const dispatch=useDispatch();
-
-  const [name, setName]=useState('');
-
-  const handleInputChange=(e)=>{
-    e.preventDefault();
-    setName(e.target.value);
-  }
-  
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    dispatch(hotelByName(name))
-    setName('');
-  } */
-
-//----------------RETURN------------------//
-/*  return (
-   <div>
-     <input type='text' placeholder='Search hotel...' onChange={handleInputChange} />
-     <button type="submit" onClick={handleSubmit}>Search</button>
-   </div>
- );
-} */
+import { hotelByName, setActualPage } from '../../redux/action/action';
 
 export default function Search() {
   const dispatch = useDispatch()
@@ -37,17 +10,20 @@ export default function Search() {
   function handleInputChange(e) {
     e.preventDefault()
     setName(e.target.value)
-    dispatch(hotelByName(name));
+    dispatch(hotelByName(name))
+    dispatch(setActualPage(1))
   }
+
   function handleSubmit(e) {
     e.preventDefault();
     dispatch(hotelByName(name));
-
+    setName('')
   }
+
   return (
     <div class="input-group ps-5" id="navbarSupportedContent">
       <div id="navbar-search-autocomplete" class="form-outline">
-        <input type="search" class="form-control " placeholder='Search hotel ...' onChange={ handleInputChange}value={name}/>
+        <input type="search" class="form-control " placeholder='Search hotel ...' name='search' onChange={ handleInputChange}/>
       </div>
       <button type='search' onClick={handleSubmit} class="btn btn-outline-primary">
         <i class="bi bi-search"></i>
