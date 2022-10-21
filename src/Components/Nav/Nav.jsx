dc4ee1abf5e5d4a2b8e6db95e73391add96309e3
solch4 <sol.maldonado.0209@gmail.com>
2022-10-21 20:05:34 -0300

    css nav updated

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,30 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux/es/exports";
 import logo from "./world-developers.png";
-import suitcase from './suitcase.svg'
+import suitcase from "./suitcase.svg";
 import "./Styles.css";
 import { useAuth } from "../../context/AuthContext";
 
 export default function Nav() {
-  const cartTotalQuantity = useSelector(state => state.reducerCart.cartTotalQuantity)
+  const cartTotalQuantity = useSelector(
+    (state) => state.reducerCart.cartTotalQuantity
+  );
 
   const { logout, user, loading } = useAuth();
+
+  const navigate = useNavigate()
 
   const handleLogout = async () => {
     try {
       await logout();
+      setUserMenuVisibility(!userMenuVisibility)
+      navigate('/')
     } catch (error) {
       console.error(error.message);
     }
   };
 
-  if (loading) return <h1>loading...</h1>
+  const [userMenuVisibility, setUserMenuVisibility] = useState(false)
+  const handleUserMenuToggle = () => setUserMenuVisibility(!userMenuVisibility)
 
+  if (loading) return <h1>loading...</h1>;
+  
   return (
     <nav className="nav-container navbar navbar-expand-lg sticky-top">
       <div className="container-fluid container-nav">
-        <Link className="navbar-brand d-flex justify-content-start align-items-center" to="/">
+        <Link
+          className="navbar-brand d-flex justify-content-start align-items-center"
+          to="/"
+        >
           <img className="navbar-logo" src={logo} alt="World Developers logo" />
           <div className="text-start">
             <h6 className="mb-0">WORLD DEVELOPERS</h6>
@@ -45,7 +57,7 @@ export default function Nav() {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <div className="mx-auto"></div>
-          <ul className="navbar-nav">
+          <ul className="navbar-nav d-flex align-items-center">
             <li className="nav-item">
               <Link className="nav-link text-dark" to="/">
                 HOME
@@ -62,22 +74,20 @@ export default function Nav() {
               </Link>
             </li>
 
-            {
-              user ?
-                <>
-                  <li className="nav-item">
-                    <Link className="nav-link text-dark" to="/home/dashboard">
-                      CREATE
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link text-dark" to="/favorite">
-                      FAVORITES
-                    </Link>
-                  </li>
-                </>
-                : null
-            }
+            {user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link text-dark" to="/home/dashboard">
+                    CREATE
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-dark" to="/favorite">
+                    FAVORITES
+                  </Link>
+                </li>
+              </>
+            )}
 
             <li className="nav-item">
               <Link className="nav-link text-dark" to="#">
@@ -86,38 +96,42 @@ export default function Nav() {
             </li>
             <li className="nav-cart-container nav-item">
               <Link to="/cart">
-                <img className="mx-3" src={suitcase} alt='Shopping Cart' />
-                <span className="nav-cart-itemsSelected">{cartTotalQuantity}</span>
+                <img className="mx-3" src={suitcase} alt="Shopping Cart" />
+                <span className="nav-cart-itemsSelected">
+                  {cartTotalQuantity}
+                </span>
               </Link>
             </li>
 
-            {
-              (!user) ?
-                <button type="button" className="btn btn-outline-primary">
-                  <Link to='/login'>
-                    LOG IN
-                  </Link>
-                </button> : null
-            }
+            {!user ? (
+              <button className="nav-log-btn ms-4" onClick={() => navigate('/login')} type="button">
+                LOG IN
+              </button>
+            ) : (
+              <div className="nav-user-container d-flex flex-column align-items-end ms-4" onClick={handleUserMenuToggle}>
+                <img
+                  src={
+                    user.photoURL
+                      ? user.photoURL
+                      : "https://www.clarkstontolldentalpractice.com/wp-content/uploads/2020/06/default-img-2-1.jpg"
+                  }
+                  alt="Profile pic"
+                />
+                <small>
+                  {user.displayName ? user.displayName : user.email}
+                </small>
+              </div>
+            )}
 
-            {
-              user ?
-                <button type="button" className="btn btn-outline-primary">
-                  <Link to='/' onClick={handleLogout}>
-                    LOG OUT
-                  </Link>
-                </button> : null
-            }
-
-            {
-              user ?
-                <div>
-                  <Link to='/profileusers' >
-                    <img src={user.photoURL ? user.photoURL : "https://www.clarkstontolldentalpractice.com/wp-content/uploads/2020/06/default-img-2-1.jpg"} class="img-circle" alt="Cinque Terre" width="50" height="50"></img>
-                  </Link>
-                  <small className="fs-6">{user.displayName ? user.displayName : user.email}</small>
-                </div> : null
-            }
+            {userMenuVisibility && (
+              <div className="nav-usermenu-bg">
+                <Link onClick={handleUserMenuToggle} to="/profileusers">See profile</Link>
+                <hr />
+                <button className="nav-log-btn" onClick={handleLogout} type="button">
+                  Log out
+                </button>
+              </div>
+            )}
           </ul>
         </div>
       </div>
