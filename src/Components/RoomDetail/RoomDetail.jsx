d02d46fabc13c90ed925bf1d265d28287bde43aa
TheOliver413 <oliverchemas@hotmail.com>
2022-10-22 00:25:58 -0500

    Logica para evitar la reserva en los mismos dias

--- src/Components/RoomDetail/RoomDetail.jsx
+++ src/Components/RoomDetail/RoomDetail.jsx
@@ -13,6 +13,7 @@ const RoomDetail = () => {
   const roomDetail = useSelector((state) => state.reducerRoom.detailRoom);
   const { name, image, price, description } = roomDetail;
   const [isFavorite, setIsFavorite] = useState(JSON.parse(localStorage.getItem('IDs'))?.includes(id))
+  const check = useSelector((state) => state.reducerCart.cartRooms);
 
   const handleFavorite = () => {
     const hasSomething = JSON.parse(localStorage.getItem('favorites'))
@@ -38,7 +39,7 @@ const RoomDetail = () => {
         toast.success(`${name} added to favorites.`, {
           position: 'bottom-right'
         })
-      } 
+      }
       //si SÍ ESTÁ en fav => la elimino
       else {
         const keepFav = hasSomething.filter(fav => fav.id !== id)
@@ -61,17 +62,40 @@ const RoomDetail = () => {
   //manejo del date input  
   const [checkIn, setCheckIn] = useState(format(new Date(), 'yyyy-MM-dd'))
   const [checkOut, setCheckOut] = useState(format(addDays(new Date(), 1), 'yyyy-MM-dd'))
-  const handleCheckInChange = (e) => setCheckIn(e.target.value)
-  const handleCheckOutChange = (e) => setCheckOut(e.target.value)
-  
-  const handleAddToCart = () => {
-    dispatch(addRoomToCart({
-      ...roomDetail,
-      totalPrice: price * differenceInDays(new Date(checkOut), new Date(checkIn)),
-      checkIn,
-      checkOut
-    }))
+
+  const handleCheckInChange = (e) => {
+    setCheckIn(e.target.value)
   }
+  const handleCheckOutChange = (e) => { setCheckOut(e.target.value) }
+
+  const handleAddToCart = () => {
+    const checkinfind = check.find(e => e.id == id)//objeto carro
+    if (checkinfind) {
+      //  2022-10-26  >=   2022-10-25      2022-10-26   <=     2022-10-29
+      if (checkIn >= checkinfind.checkIn && checkIn <= checkinfind.checkOut) {
+        console.log("Input" + checkIn)
+        console.log("Carro" + checkinfind.checkIn)
+        alert("La fecha seleccionada no esta disponible");
+      }
+      else {
+        dispatch(addRoomToCart({
+          ...roomDetail,
+          totalPrice: price * differenceInDays(new Date(checkOut), new Date(checkIn)),
+          checkIn,
+          checkOut
+        }))
+      }
+    }
+    else {
+      dispatch(addRoomToCart({
+        ...roomDetail,
+        totalPrice: price * differenceInDays(new Date(checkOut), new Date(checkIn)),
+        checkIn,
+        checkOut
+      }))
+    }
+  }
+
 
   return (
     <>
@@ -102,7 +126,7 @@ const RoomDetail = () => {
               </div>
             </div>
             <p className="mt-4">The price for {differenceInDays(new Date(checkOut), new Date(checkIn))} night/s is&nbsp;
-              <strong>${price*differenceInDays(new Date(checkOut), new Date(checkIn))}</strong>
+              <strong>${price * differenceInDays(new Date(checkOut), new Date(checkIn))}</strong>
             </p>
             {/* FALTA SERVICIOS CON ICONOS */}
 
@@ -111,7 +135,7 @@ const RoomDetail = () => {
               <button onClick={handleAddToCart} className='btn btn-primary mx-sm-2'>ADD TO CART</button>
             </p>
             <button className='btn btn-primary my-3' onClick={handleFavorite}>
-              <svg fill={isFavorite ? '#E53A27' : 'grey'} height={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
+              <svg fill={isFavorite ? '#E53A27' : 'grey'} height={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" /></svg>
             </button>
             {/* falta agregar logica al boton para que lo agregue al carrito */}
           </div>
