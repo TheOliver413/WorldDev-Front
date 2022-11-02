1eaea9925277b7c4e170317bcb2ac66811f7fd8c
TheOliver413 <oliverchemas@hotmail.com>
2022-11-02 02:57:21 -0500

    Solucion registro y boton ver contraseña

--- src/Components/Authentication/Login/Login.jsx
+++ src/Components/Authentication/Login/Login.jsx
@@ -18,6 +18,8 @@ export default function Login() {
     password: ""
   });
 
+  const [showPwd, setShowPwd] = useState(false)
+
   const { login, loginWithGoogle, resetPassword } = useAuth();
   const [error, setError] = useState("");
   const navigate = useNavigate();
@@ -34,10 +36,10 @@ export default function Login() {
       navigate('/home')
     } catch (error) {
       console.log(error.code)
-      if(error.code === 'auth/wrong-password'){
+      if (error.code === 'auth/wrong-password') {
         toast.error('Wrong password', { position: 'bottom-right' })
       }
-      if(error.code === 'auth/user-not-found'){
+      if (error.code === 'auth/user-not-found') {
         toast.error('User not found', { position: 'bottom-right' })
       }
     }
@@ -65,6 +67,7 @@ export default function Login() {
     }
   };
 
+
   return (
 
     <div className="d-flex justify-content-center align-items-center vh-100">
@@ -88,7 +91,17 @@ export default function Login() {
           <div className="input-group-text loging">
             <img src={passwordico} alt="password-icon" style={{ height: "1rem" }} />
           </div>
-          <input className="form-control bg-light" type="password" name="password" id="password" placeholder="*************" onChange={handleChange} />
+          <input className="form-control bg-light" type={showPwd ? "text" : "password"} name="password" id="password" placeholder="*************" onChange={handleChange} />
+          <div className="input-group-text loging" onClick={() => setShowPwd(!showPwd)}>
+            {showPwd ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
+              <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+              <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+            </svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
+              <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+              <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+              <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+            </svg>}
+          </div>
         </div>
 
         <div className="d-flex justify-content-around mt-1">
@@ -132,12 +145,12 @@ export default function Login() {
 
         <div className="d-flex gap-1 justify-content-center mt-1">
           <div>By signing in or creating an account, you agree to our
-          <Link to="/terms">
-            <a href="#" className="login-text text-decoration-none fw-semibold">Terms and Conditions</a>
-          </Link> and
-          <Link to="/privacy">
-            <a href="#" className="login-text text-decoration-none fw-semibold"> Privacy Policy</a>
-          </Link>
+            <Link to="/terms">
+              <a href="#" className="login-text text-decoration-none fw-semibold">Terms and Conditions</a>
+            </Link> and
+            <Link to="/privacy">
+              <a href="#" className="login-text text-decoration-none fw-semibold"> Privacy Policy</a>
+            </Link>
           </div>
         </div>
       </form>
