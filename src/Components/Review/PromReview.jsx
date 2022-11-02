402a38554cda215e426a60192b5363c3e1878b8b
Paula <pawyedro@gmail.com>
2022-11-02 03:22:29 -0300

    modificacionNav

---
+++ src/Components/Review/PromReview.jsx
@@ -0,0 +1,8 @@
+
+
+
+function PromReviewHotels() {
+    return (
+        <canvas class="my-4 w-100 chartjs-render-monitor" id="myChart" width="600" height="253" style="display: block; width: 600px; height: 253px;"></canvas>
+    )
+}
