cf876a505b010fb63d9c94b39faadb223f6c0d1a
solch4 <sol.maldonado.0209@gmail.com>
2022-11-03 14:00:22 -0300

    css login

--- src/Components/Authentication/Login/Login.jsx
+++ src/Components/Authentication/Login/Login.jsx
@@ -76,91 +76,85 @@ export default function Login() {
 
 
   return (
-
-    <div className="d-flex justify-content-center align-items-center vh-100">
+    <form onSubmit={handleSubmit} className="mx-auto my-4 bg-white p-5 rounded-5 text-secondary shadow" style={{ maxWidth: "25rem" }}>
       {error && <Alert message={error} />}
+      <div className="d-flex justify-content-center">
+        <img src={loginico} alt="login-icon" style={{ height: "7rem" }} />
+      </div>
 
-      <form onSubmit={handleSubmit} className="bg-white p-5 rounded-5 text-secondary shadow" style={{ width: "25rem" }}>
-        <div className="d-flex justify-content-center">
-          <img src={loginico} alt="login-icon" style={{ height: "7rem" }} />
+      <div className="text-center fs-1 fw-bold">Login</div>
+
+      <div className="input-group mt-4">
+        <div className="input-group-text login">
+          <img src={userico} alt="username-icon" className="user" style={{ height: "1rem" }} />
         </div>
+        <input className="form-control bg-light" type="email" name="email" id="email" placeholder="youremail@company.tld" onChange={handleChange} />
+      </div>
 
-        <div className="text-center fs-1 fw-bold">Login</div>
-
-        <div className="input-group mt-4">
-          <div className="input-group-text login">
-            <img src={userico} alt="username-icon" className="user" style={{ height: "1rem" }} />
-          </div>
-          <input className="form-control bg-light" type="email" name="email" id="email" placeholder="youremail@company.tld" onChange={handleChange} />
+      <div className="input-group mt-1">
+        <div className="input-group-text login">
+          <img src={passwordico} alt="password-icon" style={{ height: "1rem" }} />
         </div>
-
-        <div className="input-group mt-1">
-          <div className="input-group-text login">
-            <img src={passwordico} alt="password-icon" style={{ height: "1rem" }} />
-          </div>
-          <input className="form-control bg-light" type={showPwd ? "text" : "password"} name="password" id="password" placeholder="*************" onChange={handleChange} />
-          <div className="input-group-text loging" onClick={() => setShowPwd(!showPwd)}>
-            {showPwd ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
-              <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-              <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
-            </svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
-              <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
-              <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
-              <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
-            </svg>}
-          </div>
+        <input className="form-control bg-light" type={showPwd ? "text" : "password"} name="password" id="password" placeholder="*************" onChange={handleChange} />
+        <div className="input-group-text loging" onClick={() => setShowPwd(!showPwd)}>
+          {showPwd ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
+            <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+            <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+          </svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
+            <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+            <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+            <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+          </svg>}
         </div>
+      </div>
 
-        <div className="d-flex justify-content-around mt-1">
-          {/* <div className="d-flex align-items-center gap-1">
-            <input className="form-check-input" type="checkbox" />
-            <div className="pt-1" style={{ "font-size": "0.9rem" }}>Remember me</div>
-          </div> */}
+      <div className="d-flex justify-content-around mt-1">
+        {/* <div className="d-flex align-items-center gap-1">
+          <input className="form-check-input" type="checkbox" />
+          <div className="pt-1" style={{ "font-size": "0.9rem" }}>Remember me</div>
+        </div> */}
 
-          <div className="pt-1">
-            <a href="#" className="login-text text-decoration-none fw-semibold fst-italic" style={{ "font-size": "0.9rem" }} onClick={handleResetPassword}>Forgot
-              your password?</a>
-          </div>
+          <p className="login-text text-decoration-none fw-semibold fst-italic my-1" style={{ fontSize: "0.9rem" }} onClick={handleResetPassword}>
+            Forgot your password?
+          </p>
+      </div>
+
+      <div>
+        {
+          !user.email || !user.password || user.password.length < 6 ?
+            <button className="btn btn-info login text-white w-100 mt-4 fw-semibold shadow-sm" type="submit" disabled>Login</button>
+            :
+            <button className="btn btn-info login text-white w-100 mt-4 fw-semibold shadow-sm" type="submit">Login</button>
+        }
+      </div>
+
+      <div className="d-flex gap-1 justify-content-center mt-1">
+        Don't have an account?
+        <Link className="login-text text-decoration-none fw-semibold" to="/register">
+          Register
+        </Link>
+      </div>
+
+      <div className="p-3">
+        <div className="border-bottom text-center" style={{ height: "0.9rem" }}>
+          <span className="bg-white px-3">or</span>
         </div>
+      </div>
 
-        <div>
-          {
-            !user.email || !user.password || user.password.length < 6 ?
-              <button className="btn btn-info login text-white w-100 mt-4 fw-semibold shadow-sm" type="submit" disabled>Login</button>
-              :
-              <button className="btn btn-info login text-white w-100 mt-4 fw-semibold shadow-sm" type="submit">Login</button>
-          }
-        </div>
+      <div className="btn d-flex gap-2 justify-content-center border mt-3 shadow-sm">
+        <img src={googleico} alt="google-icon" style={{ height: "1.6rem" }} />
+        <div className="fw-semibold text-secondary" onClick={handleGoogleSignin}>Continue with Google</div>
+      </div>
 
-        <div className="d-flex gap-1 justify-content-center mt-1">
-          <div>Don't have an account?</div>
-          <Link to="/register">
-            <a href="#" className="login-text text-decoration-none fw-semibold">Register</a>
-          </Link>
-        </div>
-
-        <div className="p-3">
-          <div className="border-bottom text-center" style={{ height: "0.9rem" }}>
-            <span className="bg-white px-3">or</span>
-          </div>
-        </div>
-
-        <div className="btn d-flex gap-2 justify-content-center border mt-3 shadow-sm">
-          <img src={googleico} alt="google-icon" style={{ height: "1.6rem" }} />
-          <div className="fw-semibold text-secondary" onClick={handleGoogleSignin}>Continue with Google</div>
-        </div>
-
-        <div className="d-flex gap-1 justify-content-center mt-1">
-          <div>By signing in or creating an account, you agree to our
-            <Link to="/terms">
-              <a href="#" className="login-text text-decoration-none fw-semibold">Terms and Conditions</a>
-            </Link> and
-            <Link to="/privacy">
-              <a href="#" className="login-text text-decoration-none fw-semibold"> Privacy Policy</a>
-            </Link>
-          </div>
-        </div>
-      </form>
-    </div>
+      <div className="mt-3">
+        By signing in or creating an account, you agree to our&nbsp;
+        <Link className="login-text text-decoration-none fw-semibold" to="/terms">
+          Terms and Conditions
+        </Link> and&nbsp;
+        <Link className="login-text text-decoration-none fw-semibold" to="/privacy">
+          Privacy Policy
+        </Link>
+      </div>
+    </form>
   );
 }
