0316b91773feb6634c8edfd60b25861296c82e3c
solch4 <sol.maldonado.0209@gmail.com>
2022-11-04 18:06:47 -0300

    css navFilters

--- src/Components/NavFilters/NavFilters.jsx
+++ src/Components/NavFilters/NavFilters.jsx
@@ -4,15 +4,23 @@ import Order from "../Order/Order";
 import Filter from "../Filter/Filter";
 import './NavFilters.css'
 import FilterCategory from "../FilterCategory/FilterCategory";
+import { getHotels } from "../../redux/action/action";
+import { useDispatch } from "react-redux";
 
 export default function Nav() {
+  const dispatch = useDispatch()
+  const handleClearFilter = () => dispatch(getHotels())
+  
   return (
-    <div className="navfilters-container ">
+    <div className="navfilters-container d-flex flex-column flex-lg-row justify-content-lg-between align-items-center gap-1">
       <Search />
-      <div className="navfilters-order-filter d-flex justify-content-between align-items-center">
+      <div className="navfilters-order-filter d-flex flex-column flex-md-row justify-content-between justify-content-md-center align-items-center gap-1">
+        <div className="order-md-1 d-flex justify-content-between align-items-center gap-1">
+          <FilterCategory/>
+          <Filter />
+        </div>
+        <button onClick={handleClearFilter} className='navfilters-clear-btn order-md-2 filter-btn btn' type='button'>Clear filters</button>
         <Order />
-        <FilterCategory/>
-        <Filter />
       </div>
     </div>
   )
